0a2dfa69c517b7e89615f197bb573a8cdc989370
Cognify-source <fredrik.ekholm@protonmail.com>
2025-06-29 18:29:03 +0000

    fux

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}"
   ],
-  theme: {
-    extend: {
-      // Exempel på egna färger
-      colors: {
-        primary: "#123456",
-        secondary: "#abcdef"
-      },
-      // Exempel på egna typsnitt
-      fontFamily: {
-        serif: ["Merriweather", "serif"]
-      }
-    }
-  },
-  plugins: []
+  /* resten av din config */
 };
