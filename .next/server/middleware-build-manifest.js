0e46f37064a388067b4057264a7a61c90f4fd372
Cognify-source <fredrik.ekholm@protonmail.com>
2025-06-30 14:09:34 +0000

    gdgdf

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,6 +24,11 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
+    ],
+    "/for-privatpersoner": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/for-privatpersoner.js"
     ]
   },
   "ampFirstPages": []
