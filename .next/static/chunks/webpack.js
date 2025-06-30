ecfd819b25be0ea49e1efc61020731bd0c70345e
Cognify-source <fredrik.ekholm@protonmail.com>
2025-06-30 14:12:51 +0000

    gfdgfd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("cc25ef093f58a246")
+/******/ 		__webpack_require__.h = () => ("2898711b6ce6f1d2")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,38 +1,15 @@
 // src/components/Layout.js
 
-import { useEffect, useRef, useState } from 'react'
 import Navbar from './Navbar'
 
 export default function Layout({ children }) {
-  const heroRef = useRef(null)
-  const [heroHeight, setHeroHeight] = useState('auto')
-
-  useEffect(() => {
-    function updateHeight() {
-      if (heroRef.current) {
-        setHeroHeight(`${heroRef.current.clientHeight}px`)
-      }
-    }
-    // Sätt initialt
-    updateHeight()
-    // Uppdatera om fönstret ändrar storlek
-    window.addEventListener('resize', updateHeight)
-    return () => window.removeEventListener('resize', updateHeight)
-  }, [])
-
   return (
     <>
       <Navbar />
 
-      <div
-        className="flex pt-16 pb-16 bg-transparent items-stretch"
-        style={{ height: heroHeight }}
-      >
-        {/* Statisk vänsterkolumn med hero-bild */}
-        <div
-          ref={heroRef}
-          className="hidden lg:block lg:w-1/3 h-auto"
-        >
+      <div className="flex min-h-screen pt-16 pb-16">
+        {/* 1) Statisk vänsterkolumn med hero-bild */}
+        <div className="hidden lg:block lg:w-1/3 h-full">
           <img
             src="/images/hero.png"
             alt="Hero"
@@ -40,18 +17,18 @@ export default function Layout({ children }) {
           />
         </div>
 
-        {/* Dynamisk mittkolumn */}
+        {/* 2) Dynamisk mittkolumn – samma höjd som hero (minst viewport height) */}
         <main className="w-full lg:w-1/3 px-6 py-8 flex flex-col items-center justify-center">
           <div className="w-full text-black">
             {children}
           </div>
 
-          {/* Testimonials-sektionen */}
+          {/* Testimonials (kan tas bort om du vill) */}
           <section className="w-full mt-12 relative">
             <button className="left-arrow absolute left-0 top-1/2 transform -translate-y-1/2 text-2xl text-black">
               ‹
             </button>
-            <div className="testimonial-slide relative h-32 overflow-hidden">
+            <div className="testimonial-slide relative h-32 overflow-hidden text-black">
               {/* laddas in via fetch */}
             </div>
             <button className="right-arrow absolute right-0 top-1/2 transform -translate-y-1/2 text-2xl text-black">
@@ -60,7 +37,7 @@ export default function Layout({ children }) {
           </section>
         </main>
 
-        {/* Tom högra kolumn för horisontell centrering */}
+        {/* 3) Tom högra kolumn för horisontell centrering */}
         <div className="hidden lg:block lg:w-1/3" />
       </div>
     </>
