fedf99910056552a3469368f64e893512664d5fe
Cognify-source <fredrik.ekholm@protonmail.com>
2025-07-02 07:24:32 +0000

    dfsd

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,11 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-4xl font-serif font-extrabold text-gray-800 mb-4 text-left">
-        Utforska dina inre delar
-        – hitta tillbaka till ditt autentiska jag.
+      <h1 className="text-4xl font-serif font-bold italic text-gray-800 mb-4 text-left">
+        Utforska dina inre delar – hitta tillbaka till ditt autentiska jag.
       </h1>
       <p className="text-lg text-gray-600 mb-6">
-        Känner du att olika sidor av dig drar åt olika håll? I min coaching får du möta och integrera dina inre delar så att de kan sam­arbeta i stället för att konkurrera. Min coaching bygger på <i>Internal Family Systems (IFS)</i> som grund men är kompletterad detta med andra verktyg som stödjer just din resa.
+        Känner du att olika sidor av dig drar åt olika håll? I min coaching får du möta och integrera dina inre delar så att de kan sam­arbeta i stället för att konkurrera. Min coaching bygger på <i>Internal Family Systems (IFS)</i> som grund och kompletterar detta med andra verktyg som stödjer just din resa.
       </p>
       <button className="px-6 py-3 border border-gray-400 text-gray-700 font-medium hover:bg-gray-200 transition">
         BOKA ETT KOSTNADSFRITT INTRODUKTIONSSAMTAL
