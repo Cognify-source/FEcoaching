6d30d32bdeaea9ad8ab6618d92a5018e0e40ac40
Cognify-source <fredrik.ekholm@protonmail.com>
2025-07-06 18:33:08 +0000

    fdsfdfs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -129,7 +129,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("20331953ad3aef18")
+/******/ 		__webpack_require__.h = () => ("1bfa77bef85b0648")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
